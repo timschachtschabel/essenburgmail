5543d546c95e362a418562b808783d7c4b261aef
D214466 <D214466@edu.rocwb.nl>
2017-03-08 16:02:11 +0100

    login front end demo

---
+++ essenburgmailer/resources/views/login.blade.php
@@ -0,0 +1,19 @@
+@extends('components.head')
+
+    <div class="login col-lg-6 col-md-offset-3">
+        <form action="">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control" type="text" id="email" name="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password" id="password">
+            </div>
+            <div class="form-group">
+                <input class="btn btn-primary" type="submit" value="login" name="login">
+            </div>
+        </form>
+    </div>
+
+@extends('components.foot')
