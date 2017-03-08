a5aca00c19b7d8a97774ddf259a53f19dd3b9ba9
DennisKrijgsman <Dennis.krijgsman@hotmail.com>
2017-03-08 15:25:52 +0100

    Welcome blade
    blade shit

--- essenburgmailer/resources/views/welcome.blade.php
+++ essenburgmailer/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">asdfghjhgfdew 5</div>
+                <div class="title">Welcome tim to my fancy fcking website 5</div>
             </div>
         </div>
     </body>
